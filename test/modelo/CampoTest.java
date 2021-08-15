5d0a35cd32e31ec721e9eea9b73a5976b7caaff6
Thalita Ribeiro <sribeiro.thalita@gmail.com>
2021-08-15 15:43:02 -0300

    Campo minado

--- test/modelo/CampoTest.java
+++ test/modelo/CampoTest.java
@@ -1,4 +1,30 @@
-import static org.junit.jupiter.api.Assertions.*;
+package modelo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class CampoTest {
-  
+    private Campo campo;
+
+    @BeforeEach
+    void iniciarCampo() {
+        campo = new Campo(3, 3);
+    }
+
+    @Test
+    void testeVizinhoRealDistancia1() {
+        Campo vizinhoEsquerda = new Campo(3, 2);
+        Campo vizinhoDireita = new Campo(4, 4);
+        Campo naoVizinho = new Campo(0, 0);
+
+        boolean resultadoEsquerda = campo.adicionarVizinho(vizinhoEsquerda);
+        boolean resultadoDireita = campo.adicionarVizinho(vizinhoDireita);
+        boolean resultado = campo.adicionarVizinho(naoVizinho);
+        assertTrue(resultadoEsquerda);
+        assertTrue(resultadoDireita);
+        assertFalse(resultado);
+    }
 }
